68d2ac0aa82a08650f75af9e27efb00a5d37419c
Valera <ditrikh.valeriia@lll.kpi.ua>
2025-04-07 19:24:57 +0300

    Added light

---
+++ Horror_game/Assets/Scripts/Play_music_Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip firstSound;
+    public AudioClip secondSound;
+
+    private bool firstSoundPlayed = false;
+
+    public void PlayFirstSound()
+    {
+        audioSource.clip = firstSound;
+        audioSource.Play();
+    }
+
+    public void PlaySecondSound()
+    {
+        audioSource.clip = secondSound;
+        audioSource.Play();
+        StartCoroutine(ClearAudioClipAfterPlay(secondSound.length));
+    }
+
+    private System.Collections.IEnumerator ClearAudioClipAfterPlay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        audioSource.clip = null;
+    }
+}
