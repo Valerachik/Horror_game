cb5294153d4f445fb49ed226fa1006aae4bf5b9f
Valera <ditrikh.valeriia@lll.kpi.ua>
2025-05-30 19:58:32 +0300

    Added screamers

--- Horror_game/Assets/Scripts/movedoor.cs
+++ Horror_game/Assets/Scripts/movedoor.cs
@@ -7,16 +7,33 @@ public class movedoor : MonoBehaviour
     private bool isOpen = false;
     private Quaternion startRotation;
     private Quaternion openRotation;
+    public GameObject screamObject;
+    private Animator screamAnimator;
+    private bool scremer = false;
 
     void Start()
     {
         startRotation = transform.localRotation;
         openRotation = Quaternion.Euler(0, openAngle, 0) * startRotation;
+        if (screamObject != null)
+        {
+            screamObject.SetActive(false);
+        }
+        if (screamObject != null && !scremer)
+        {
+            screamAnimator = screamObject.GetComponent<Animator>();
+            screamObject.SetActive(false);
+        }
     }
 
     void OnMouseDown()
     {
         isOpen = !isOpen; // Перемикаємо стан дверей
+
+        if (CompareTag("door_trig") && !scremer)
+        {
+            ShowScreamer();
+        }
     }
 
     void Update()
@@ -28,6 +45,22 @@ public class movedoor : MonoBehaviour
         else
         {
             transform.localRotation = Quaternion.Lerp(transform.localRotation, startRotation, Time.deltaTime * speed);
+        }
+    }
+    void ShowScreamer()
+    {
+        if (screamObject != null)
+        {
+            screamObject.SetActive(true);
+            screamAnimator.Play("ScreamAnim", 0, 1f);
+            scremer = true;
+        }
+    }
+    public void OnScreamAnimationEnd()
+    {
+        if (screamObject != null)
+        {
+            screamObject.SetActive(false);
         }
     }
 }
